631411ce8c58ea2e8a131910ace32ee370d4644f
@billionairedev <pius622472@gmail.com>
2026-09-18 10:54:42 +0100

    Time: 3 ms (97.52%), Space: 21.1 MB (15.24%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,12 +1,10 @@
 class Solution:
     def findErrorNums(self, nums: list[int]) -> list[int]:
-        nlist = nums
-        missing = 0
-        for i in range(1, len(nums)+1):
-             #fixedlist.append(i)
-            if i not in nums:
-                missing = i
-            if i in nlist:
-                nlist.remove(i)
-        nlist.append(missing)
-        return nlist
+        n = len(nums)
+        actualSum = sum(nums)
+        uniqueSum = sum(set(nums))
+        expectedSum = n * (n+1) // 2
+        duplicate = actualSum - uniqueSum
+        missing = expectedSum - uniqueSum
+
+        return [duplicate, missing]
